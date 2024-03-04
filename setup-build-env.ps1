d8d0e8b864798e39a2bb6383501f4a3525072d95
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-03-04 09:57:52 -0800

    chore: Reformat setup-build-env.ps1

--- setup-build-env.ps1
+++ setup-build-env.ps1
@@ -2,26 +2,27 @@
 ##
 ##  DAX Studio - build environment setup script.
 ##
-## this script copies a couple of dlls into the 
+## this script copies a couple of dlls into the
 ## a lib folder so that DaxStudio will build.
 ## =====================================================
 
-cls
-
+Clear-Host
 
 ## Declare the required dll's and search folders
 
-$requiredDlls = @("Microsoft.Excel.AdomdClient.dll"
-, "Microsoft.Excel.Amo.dll"
-, "Microsoft.AnalysisServices.AdomdClient.dll")
+$requiredDlls = @(
+    'Microsoft.Excel.AdomdClient.dll'
+    , 'Microsoft.Excel.Amo.dll'
+    , 'Microsoft.AnalysisServices.AdomdClient.dll')
 
-$searchFolders = @("${Env:ProgramFiles(x86)}\Common Files\Microsoft Shared\Office15\DataModel\"
-, "$Env:ProgramFiles\Microsoft Office 15\root\vfs\ProgramFilesCommonX86\Microsoft Shared\OFFICE15\DataModel\"
-, "$Env:ProgramFiles\Common Files\microsoft shared\OFFICE15\DataModel"
-, "$Env:ProgramFiles\Common Files\microsoft shared\OFFICE16\DataModel"
-, "$Env:ProgramFiles\Microsoft.NET\ADOMD.NET\110\"
-, "$Env:ProgramFiles\Microsoft.NET\ADOMD.NET\120\"
-, "$Env:ProgramFiles\Microsoft.NET\ADOMD.NET\150\"
+$searchFolders = @(
+    "${Env:ProgramFiles(x86)}\Common Files\Microsoft Shared\Office15\DataModel\"
+    , "$Env:ProgramFiles\Microsoft Office 15\root\vfs\ProgramFilesCommonX86\Microsoft Shared\OFFICE15\DataModel\"
+    , "$Env:ProgramFiles\Common Files\microsoft shared\OFFICE15\DataModel"
+    , "$Env:ProgramFiles\Common Files\microsoft shared\OFFICE16\DataModel"
+    , "$Env:ProgramFiles\Microsoft.NET\ADOMD.NET\110\"
+    , "$Env:ProgramFiles\Microsoft.NET\ADOMD.NET\120\"
+    , "$Env:ProgramFiles\Microsoft.NET\ADOMD.NET\150\"
 )
 
 ## 1. Create lib subfolder
@@ -30,42 +31,37 @@ $ScriptRoot = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
 
 $libPath = "$ScriptRoot\lib"
 
-if (-not (Test-Path $libPath))
-{
-	mkdir $libPath | Out-Null	
+if (-not (Test-Path $libPath)) {
+    mkdir $libPath | Out-Null
 }
 
 ## 2. Try to copy the dlls by searching for it in all the declared folders
 
-$searchFolders |% {
-	
-	$folder = $_
-	
-	$requiredDlls |% {
-	
-		$dllName = $_
-		
-		$dllPath = Join-Path $folder $dllName		
-		
-		if (Test-Path $dllPath)
-		{
-			$newDllPath = Join-Path $libPath $dllName
-			
-			Copy-Item $dllPath $newDllPath -Verbose
-		}		
-	}	
+$searchFolders | ForEach-Object {
+    $folder = $_
+
+    $requiredDlls | ForEach-Object {
+
+        $dllName = $_
+
+        $dllPath = Join-Path $folder $dllName
+
+        if (Test-Path $dllPath) {
+            $newDllPath = Join-Path $libPath $dllName
+
+            Copy-Item $dllPath $newDllPath -Verbose
+        }
+    }
 }
 
 ## 3. Confirms if all the dlls had been copied
 
-$dllsOnLibFolder = gci -Path $libPath -Filter "*.dll" | select  -ExpandProperty Name
+$dllsOnLibFolder = Get-ChildItem -Path $libPath -Filter '*.dll' | Select-Object -ExpandProperty Name
 
-$requiredDlls |% {
-	
-	if (-not ($dllsOnLibFolder -contains $_))
-	{
-		Write-Warning "Cannot find dependency: $($_)" 
-	}
-	
+$requiredDlls | ForEach-Object {
+    if (-not ($dllsOnLibFolder -contains $_)) {
+        Write-Warning "Cannot find dependency: $($_)"
+    }
+
 }
 
