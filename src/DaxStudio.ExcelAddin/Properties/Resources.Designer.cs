f57c42fd0a77e3090e6525b8c3c57b67f34cdf6d
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-03-09 14:45:33 -0800

    Closer to fix

--- src/DaxStudio.ExcelAddin/Properties/Resources.Designer.cs
+++ src/DaxStudio.ExcelAddin/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace DaxStudio.Properties {
+namespace ExcelAddIn1.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace DaxStudio.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("DaxStudio.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ExcelAddIn1.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- src/DaxStudio.ExcelAddin/Properties/Settings.Designer.cs
+++ src/DaxStudio.ExcelAddin/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace DaxStudio.Properties {
+namespace ExcelAddIn1.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
