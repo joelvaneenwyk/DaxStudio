09f0bd783415b9640f4bc7f8749ad5dc5e824cd5
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-03-09 20:42:29 -0800

    Run a batch script to output build time instead of using powershell

--- src/DaxStudio.Standalone/GenerateBuildTime.cmd
+++ src/DaxStudio.Standalone/GenerateBuildTime.cmd
@@ -6,4 +6,3 @@ goto:$Main
     set "_project_dir=%~dp0"
     call "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe" -Command "((Get-Date).ToUniversalTime()).ToString() | Out-File "%_project_dir%Resources\BuildDate.txt" -NoNewline -Encoding ASCII"
 endlocal & exit /b %errorlevel%
-
